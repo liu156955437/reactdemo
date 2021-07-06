ab5c1b3c7db408c503cee30814709b0b68e099b1
liuhh <liuhh@zijingkeji.com>
2021-07-06 16:39:27 +0800

    验证

--- src/views/login/LoginForm.js
+++ src/views/login/LoginForm.js
@@ -4,6 +4,8 @@ import "./Index.scss";
 import { Form, Input, Button,Row, Col} from 'antd';
 import { UserOutlined,LockOutlined} from '@ant-design/icons';
 
+//验证
+import  {validate_password} from "../../utils/vaildate";
 ///登录
 class LoginForm extends Component {
     constructor() {
@@ -28,20 +30,47 @@ class LoginForm extends Component {
                     <span onClick={this.toggleForm}>账号注册</span>
                 </div>
                 <div className="form-content">
-                    <Form name="normal_login" className="login-form" initialValues={{ remember: true }} onFinish={()=>this.onFinish} >
-                    <Form.Item name="username" rules={[{ required: true, message: '请输入有效的用户名' }]} >
-                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                    <Form name="normal_login" className="login-form" initialValues={{ remember: true }} onFinish={this.onFinish} >
+                    <Form.Item name="username" rules={
+                        [
+                            { required: true, message: '邮箱不能为空！' },
+                            {type:"email",message:"邮箱格式不正确"}
+                        ]
+                        } >
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="email" />
                     </Form.Item>
-                    <Form.Item name="password" rules={[{ required: true, message: '请输入有效的密码！' }]} >
+                    <Form.Item name="passwod" rules={
+                        [
+                            { required: true, message: '密码不能为空' },
+                            // ({ getFieldValue }) => ({//Es6结构     自定义属性
+                            //     validator(rules, values) {
+                            //       if (values.length<6) {
+                            //           return Promise.reject('不能小于6位')
+                            //       }else{
+                            //           return Promise.reject();
+                            //       }
+                    
+                            //      // return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                            //     },
+                            //   }),
+                            // {min:6,message:'密码不能小于6位'},
+                            // {max:10,message:'密码不能大于10位'},
+                            {pattern: validate_password,message:"请输入大于6位小于20位数字和字母"},
+                        ]
+                        } >
                         <Input prefix={<LockOutlined className="site-form-item-icon" />} placeholder="Password" />
                     </Form.Item>
-                    <Form.Item name="password" rules={[{ required: true}]} >
+                    <Form.Item name="code" rules={
+                        [
+                            { required: true,message:"验证码不能为空"},
+                            {len:6,message:"请输入长度为6位的验证码"}
+                        ]} >
                         <Row gutter={13}>
                             <Col span={15}>
                                 <Input prefix={<LockOutlined className="site-form-item-icon"/>} placeholder="code" block/>
                             </Col>
                             <Col span={9}>
-                                <Button type="danger" block>获取验证码</Button>
+                                <Button htmltype="danger" block>获取验证码</Button>
                             </Col>
                         </Row>
                     </Form.Item>
